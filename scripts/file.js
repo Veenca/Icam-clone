143b9057a0978abd93b33fa8e4290476537d7a3b
Veenca <63163300+Veenca@users.noreply.github.com>
2020-09-25 18:00:34 +0200

    nav bar almost done i only need to add the ajax popup

--- scripts/file.js
+++ scripts/file.js
@@ -1,1 +1,17 @@
-console.log($('html').width())
+let strcss;
+$('#main_nav>li').on('click',function(){
+    strcss=$(this).children().attr('id');
+    if(strcss!=undefined){
+    strcss=strcss.slice(strcss.length-1,strcss.lenght);
+    console.log(strcss);
+    }
+    for(let i=0;i<=3;i++){
+        if(i!=strcss){
+        if(!($('#menu'+i).hasClass("hide"))){
+            $('#menu'+i).addClass("hide");
+        }
+    }
+}
+    $('#menu'+strcss).toggleClass("hide");
+
+})
