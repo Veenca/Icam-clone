bdc90f00e19f461a313430c4587291b2183e5333
Veenca <63163300+Veenca@users.noreply.github.com>
2020-09-30 20:43:28 +0200

    updated nav bar , now you can click links and open ajax tab

--- scripts/file.js
+++ scripts/file.js
@@ -3,7 +3,7 @@ $('#main_nav>li').on('click',function(){
     strcss=$(this).children().attr('id');
     if(strcss!=undefined){
     strcss=strcss.slice(strcss.length-1,strcss.lenght);
-    console.log(strcss);
+  
     }
     for(let i=0;i<=3;i++){
         if(i!=strcss){
@@ -36,32 +36,44 @@ var mySwiper = new Swiper('.swiper-container', {
     // And if we need scrollbar
     
   })
-  //AJAX
+//Open Ajax Window
   
   const xhr =new XMLHttpRequest();
   
-//Open Ajax Window
-let popupBtn = $('.icam-container-items>ul>li');
-let exitPopup =$('#exit-icn');
-popupBtn.on('click',function(){
-   $('.popup-window').css("display","block");
-    let par=$(this).text().toLowerCase();
-    $(this).parent().parent().parent().toggleClass("hide")
+
+
+function serverResponse(par){
+    $('.popup-window').css("display","block");
+   
+  
+   
    xhr.onreadystatechange=function (){
-     
-    if(xhr.readyState==4 ){
+  
+    if(xhr.readyState==4 && xhr.status==200 ){
       
-        if(xhr.status==200){ $('.popup-content').html(xhr.responseText);
+       $('.popup-content').html(xhr.responseText);
       
-      }else if(xhr.status==404){
-          console.log('File or resource was not found');
-      }
+      
     
-    }
+    }else if(xhr.status==404){
+      console.log('File or resource was not found');
+  }
   };
   xhr.open('get','other-pages/'+par+'.html',true);
   xhr.send();
+  
+}
+
+let popupBtn = $('.icam-container-items>ul>li');
+
+let exitPopup =$('#exit-icn');
+popupBtn.on('click',function(){
+    $('.popup-content').html('');
+    $(this).parent().parent().parent().toggleClass("hide");
+    let par=$(this).text().toLowerCase();
+    serverResponse(par);
 });
+
 exitPopup.on('click',function(){
     $('.popup-window').css("display","none");
 });
